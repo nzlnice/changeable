6df3e97104115a4eb2c626fef66f3fa2ea228a79
nzlnice <99152724+nzlnice@users.noreply.github.com>
2024-02-02 22:54:51 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,25 +11,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-impl.recovery \
-    
-
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
 
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 29
 # VNDK API
 PRODUCT_TARGET_VNDK_VERSION := 30
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
-
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# otacert
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(DEVICE_PATH)/security/vivo_releasekey
-
-# PRODUCT_RELEASE_NAME ro.twrp.device.name
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.device.name=$(PRODUCT_RELEASE_NAME)
